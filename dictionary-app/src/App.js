5604b2a3b1eb34bb0260d56afbcf1f935e4f2172
Kingsley Atuba <atubak40@gmail.com>
2021-08-04 10:47:18 +0100

    API imported with axiox, USESTATE and USEEFFECT. Background Color set

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { Container } from '@material-ui/core'
+
+function App() {
+  const [word, setWord] = useState('')
+  const [meanings, setMeanings] = useState([])
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        'https://api.dictionaryapi.dev/api/v2/entries/en/plane'
+      )
+
+      console.log(data)
+      setMeanings(data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(meanings)
+
+  useEffect(() => {
+    dictionaryApi()
+  }, [])
+  return (
+    <div className='App'>
+      <Container className='container' maxWidth='md'>
+        Dictionary
+      </Container>
+    </div>
+  )
+}
+
+export default App
