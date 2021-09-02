5b1f6a5e3019c0473dfad8edb15ee161b0e7ead9
Kingsley Atuba <atubak40@gmail.com>
2021-09-02 09:30:07 +0100

    dictionary body completed with beatiful UI

--- dictionary-app/src/components/Definitions/Definitions.js
+++ dictionary-app/src/components/Definitions/Definitions.js
@@ -7,7 +7,31 @@ const Definitions = ({ word, category, meanings }) => {
       {word === '' ? (
         <span className='subTitle'>Start by typing a word in search</span>
       ) : (
-        'Something'
+        meanings.map((mean) =>
+          mean.meanings.map((item) =>
+            item.definitions.map((def) => (
+              <div
+                className='singleMean'
+                style={{ backgroundColor: 'white', color: 'black' }}
+              >
+                <b>{def.definition}</b>
+                <hr style={{ backgroundColor: 'black', width: '100%' }} />
+                {def.example && (
+                  <span>
+                    <b>Example: </b>
+                    {def.example}
+                  </span>
+                )}
+                {def.synonyms && (
+                  <span>
+                    <b>Synonyms: </b>
+                    {def.synonyms.map((s) => `${s}, `)}
+                  </span>
+                )}
+              </div>
+            ))
+          )
+        )
       )}
     </div>
   )
