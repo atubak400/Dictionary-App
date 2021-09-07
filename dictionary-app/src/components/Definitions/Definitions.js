f6379d32341f12196fc0fa2681ac260b31b598f0
Kingsley Atuba <atubak40@gmail.com>
2021-09-07 10:56:05 +0100

    dictionary audio done

--- dictionary-app/src/components/Definitions/Definitions.js
+++ dictionary-app/src/components/Definitions/Definitions.js
@@ -4,6 +4,16 @@ import './definitions.css'
 const Definitions = ({ word, category, meanings }) => {
   return (
     <div className='meanings'>
+      {meanings[0] && word && category === 'en' && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ backgroundColor: 'fff', borderRadius: 10 }}
+          controls
+        >
+          Your Browser doesn't support audio element.
+        </audio>
+      )}
+
       {word === '' ? (
         <span className='subTitle'>Start by typing a word in search</span>
       ) : (
